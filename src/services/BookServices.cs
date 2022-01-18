29328a20ce354c25ae85a53620a3907153e6f926
Khoa <dangkhoa123bn@gmail.com>
2022-01-18 22:41:08 +0700

    update book services

--- src/services/BookServices.cs
+++ src/services/BookServices.cs
@@ -16,45 +16,7 @@ namespace Library_Management.src.services
         {
             db = new libraryEntities();
         }
-        private SqlConnection sqlConn = Connectdb.getConn();
-        public bool addBook(Book book)
-        {
-            int quanlityBook = getQuanlityBook();
-            String query = String.Format("insert into SACH(MaSach,TenSach,SoLuong,TacGia,NamXuatBan,NhaXuatBan) values('{0}','{1}','{2}','{3}','{4}','{5}')","S"+quanlityBook+1,book.BookName,book.Quantily,book.Author,book.PublishingYear,book.Publisher);
-            SqlCommand sqlCommand = new SqlCommand(query,sqlConn);
-            sqlConn.Open();
-            try
-            {
-                sqlCommand.ExecuteNonQuery();
-                sqlConn.Close();
-                return true;
 
-            }
-            catch (SqlException ex)
-            {
-                Console.WriteLine(ex.Message);
-                sqlConn.Close();
-                return false;
-            }
-        }
-
-        public int getQuanlityBook()
-        {
-            String query = "Select Count(MaSach) from SACH";
-            int quanlity = 0;
-            try
-            {
-                SqlCommand sqlCommand = new SqlCommand(query, sqlConn);
-                sqlConn.Open();
-                quanlity = (int)sqlCommand.ExecuteScalar();               
-            }
-            catch (SqlException ex)
-            {
-                MessageBox.Show(ex.Message);
-            }
-            sqlConn.Close();
-            return quanlity;
-        }
 
         public dynamic getListBook()
         {
@@ -68,57 +30,11 @@ namespace Library_Management.src.services
             }).ToList();
             return ds;
         }
-        public Book getBookById(String id)
-        {
-            String query = String.Format("Select * from SACH where MaSach='{0}'", id);
-            Book book=null;
-            SqlCommand sqlCommand = new SqlCommand(query, sqlConn);
-            try
-            {
-                sqlConn.Open();
-                var reader = sqlCommand.ExecuteReader();
-                if (reader.HasRows)
-                {
-                    while (reader.Read())
-                    {
-                        // Đọc từng dòng tập kết quả
-                        book = new Book(reader.GetString(0), reader.GetString(1), reader.GetInt32(2), reader.GetString(3), reader.GetInt32(4), reader.GetString(5));                   
-                    }
-                   }
-            }
-            catch(SqlException ex)
-            {
-                MessageBox.Show(ex.Message);
-            }
-            sqlConn.Close();
-            return book;
-        }
 
-        public Book getBookByName(String name)
-        {
-            String query = String.Format("Select * from SACH where TenSach=N'{0}'", name);
-            Book book = null;
-            SqlCommand sqlCommand = new SqlCommand(query, sqlConn);
-            try
-            {
-                sqlConn.Open();
-                var reader = sqlCommand.ExecuteReader();
-                if (reader.HasRows)
-                {
-                    while (reader.Read())
-                    {
-                        // Đọc từng dòng tập kết quả
-                        book = new Book(reader.GetString(0), reader.GetString(1), reader.GetInt32(2), reader.GetString(3), reader.GetInt32(4), reader.GetString(5));
-                    }
-                }
-            }
-            catch (SqlException ex)
-            {
-                MessageBox.Show(ex.Message);
-            }
-            sqlConn.Close();
-            return book;
-        }
+
+
+
+
     }
 
 }
